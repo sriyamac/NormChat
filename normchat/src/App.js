d85d44e3a26e4c48035083cfac2ef5e169260cab
Christian Browning <christianbrowning84@gmail.com>
2024-02-21 17:58:42 -0500

    NormChat React App Creation
    i created a react app and cleared it out. So it is ready for implementation and what not

---
+++ normchat/src/App.js
@@ -0,0 +1,16 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div>
+        <h1>NormChat</h1>
+        <p> sup y'all</p>
+      </div>
+
+    </div>
+  );
+}
+
+export default App;
