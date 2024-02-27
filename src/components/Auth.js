119078560b80fa0b7d96c5ad206041b8fa769ca3
Christian Browning <christianbrowning84@gmail.com>
2024-02-26 23:39:23 -0500

    Add new files and update configurations. Refaced auth screen

--- src/components/Auth.js
+++ src/components/Auth.js
@@ -3,6 +3,27 @@ import { signInWithPopup } from "firebase/auth";
 import "../styles/App.css";
 
 import Cookies from "universal-cookie";
+
+import { Button, buttonVariants } from "../components/ui/button"
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "../components/ui/card"
+import { Input } from "../components/ui/input"
+import { Label } from "../components/ui/label"
+import { Link } from "react-router-dom"
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "../components/ui/select"
+import { EnvelopeOpenIcon } from "@radix-ui/react-icons"
 const cookies = new Cookies();//get, set, and remove cookies from browser
 
 export const Auth = ({ setIsAuth, setRoom }) => {
@@ -24,8 +45,39 @@ export const Auth = ({ setIsAuth, setRoom }) => {
   
   return (
     <div className="auth">
-      <p> Sign In With Google To Continue </p>
+      <Card className="w-[350px]">
+      <CardHeader>
+        <CardTitle> Sign In</CardTitle>
+        <CardDescription>Please Sign In with Google to Continue.</CardDescription>
+      </CardHeader>
+      <CardContent>
+        
+          <div  className="grid w-full items-center gap-4">
+            <div className="flex flex-col space-y-1.5">
+            <Button >   
+            <EnvelopeOpenIcon className="mr-5 h-4 w-4" />  
+            <p onClick={signInWithGoogle}>Sign in with Email</p>
+               </Button>
+
+           
+            
+          
+
+           
+            </div>
+    
+            
+          </div>
+        
+      </CardContent>
+      <CardFooter className="flex justify-between">
+       
+      </CardFooter>
+    </Card>
+ 
+      {/* <p> Sign In With Google To Continue </p>
       <button onClick={signInWithGoogle}> Sign In With Google </button>
+    </div> */}
     </div>
   );
 };
