6aad8eb850c73996bd463ed75a6f8008e231132d
Sriya <79619033+sriyamac@users.noreply.github.com>
2024-03-01 03:01:32 -0500

    commented code that works, current UI is blocking conversation list to populate

--- src/components/ConversationList.js
+++ src/components/ConversationList.js
@@ -59,3 +59,53 @@ export const ConversationList = () => {
 };
 
 export default ConversationList;
+
+// Conversation List logic that works but with no UI: 
+
+// import React, { useState, useEffect } from 'react';
+// import { Link } from 'react-router-dom';
+// import { collection, query, orderBy, getDocs, limit } from 'firebase/firestore';
+// import { db } from '../firebase-config';
+
+// export const ConversationList = () => {
+//   const [closedRooms, setClosedRooms] = useState([]);
+
+//   useEffect(() => {
+//     const fetchClosedRooms = async () => {
+//       const roomsCollection = collection(db, 'messages');
+//       const closedRoomsQuery = query(roomsCollection, orderBy('createdAt', 'desc'), limit(1));
+
+//       try {
+//         const closedRoomsSnapshot = await getDocs(closedRoomsQuery);
+//         const closedRoomsData = closedRoomsSnapshot.docs.map((doc) => ({
+//           id: doc.id,
+//           room: doc.data().room, //room reference 
+//           lastMessage: doc.data().text, // the last message the user sent in the room
+//         }));
+//         setClosedRooms(closedRoomsData);
+//       } catch (error) {
+//         console.error('Error fetching closed rooms:', error);
+//       }
+//     };
+//     fetchClosedRooms();
+//   }, []);
+//   return (
+//     <div className="chat-app">
+//       <div className="header">
+//       <h1>Conversation List</h1>
+//       </div>
+//       <div className="messages">
+//       <ul>
+//         {closedRooms.map((closedRoom) => (
+//           <li key={closedRoom.id}>
+//             <Link to={`/conversation-list/${closedRoom.id}`}>
+//               <strong>{`Room:`}</strong> {closedRoom.room}, <strong>{`Last Message:`}</strong> {closedRoom.lastMessage}
+//             </Link>
+//           </li>
+//         ))}
+//       </ul>
+//       </div>
+//     </div>
+//   );
+// };
+// export default ConversationList;
