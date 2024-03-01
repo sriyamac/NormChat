80b6ca52723c3f72b43c59cbdc73f09bf2c21d18
Giovanni A <32625900+gabbate102@users.noreply.github.com>
2024-03-01 14:13:12 -0500

    Update Feedback component with form logic and database integration

--- src/components/Feedback.js
+++ src/components/Feedback.js
@@ -5,6 +5,7 @@ export const Feedback = () => {
   To do: 
   - Change back icon to an X
   - Add form logic
+    - possibly create table in db for form data and save the feedback to the db? 
   */
   return (
     <>
@@ -19,14 +20,14 @@ export const Feedback = () => {
           <form className="flex flex-column w-[70%] justify-center items-center w-[50%] border rounded p-4 shadow-sm">
             <label for="rating" className="text-lg">Rate NormChat</label>
             <div className="rating gap-1 mb-4 mt-2" id="rating">
+              <input type="radio" name="rating-1" className="mask mask-heart bg-red-400" />
+              <input type="radio" name="rating-2" className="mask mask-heart bg-red-400" />
               <input type="radio" name="rating-3" className="mask mask-heart bg-red-400" />
-              <input type="radio" name="rating-3" className="mask mask-heart bg-red-400" />
-              <input type="radio" name="rating-3" className="mask mask-heart bg-red-400" />
-              <input type="radio" name="rating-3" className="mask mask-heart bg-red-400" />
-              <input type="radio" name="rating-3" className="mask mask-heart bg-red-400" />
+              <input type="radio" name="rating-4" className="mask mask-heart bg-red-400" />
+              <input type="radio" name="rating-5" className="mask mask-heart bg-red-400" />
             </div>
             <label for="text-response" className="text-lg">Tell us what you think</label>
-            <textarea className="textarea textarea-bordered p-1.5 h-40 w-[100%] mt-2" id="text-response" placeholder="What do you think?"></textarea>
+            <textarea className="textarea textarea-bordered p-1.5 h-40 w-[100%] mt-2 shadow-inner" id="text-response" placeholder="What do you think?"></textarea>
             <button className="btn btn-success mt-4" type="submit">Submit</button>
           </form>
         </div>
