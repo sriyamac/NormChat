a8647a6d1b7078d74fa70f8cdb4a09ec25862947
Giovanni Abbate <gabbate2011@gmail.com>
2024-03-12 14:59:09 -0400

    Fixed dark input box issue

--- tailwind.config.js
+++ tailwind.config.js
@@ -83,8 +83,7 @@ module.exports = {
   },
   plugins: [require("daisyui")],
   daisyui: {
-    themes: false, // false: only light + dark | true: all themes | array: specific themes like this ["light", "dark", "cupcake"]
-    darkTheme: "dark", // name of one of the included themes for dark mode
+    themes: ["light"], // false: only light + dark | true: all themes | array: specific themes like this ["light", "dark", "cupcake"]
     base: true, // applies background color and foreground color for root element by default
     styled: true, // include daisyUI colors and design decisions for all components
     utils: true, // adds responsive and modifier utility classes
