bef05fbc8618604668f5eb1a45277b99a462f8fc
Giovanni A <32625900+gabbate102@users.noreply.github.com>
2024-03-31 15:30:07 -0400

    Added conditional rendering to auth.js

--- src/components/Auth.js
+++ src/components/Auth.js
@@ -3,12 +3,13 @@ import { signInWithPopup } from "firebase/auth";
 import "../styles/App.css";
 import Cookies from "universal-cookie";
 import { Card } from "../components/ui/card";
-import { Link } from 'react-router-dom';
+import { Signup } from './Signup.js'
 
 
 const cookies = new Cookies(); //get, set, and remove cookies from browser
 
 export const Auth = ({ setIsAuth, setRoom }) => {
+  let show = false;
   const signInWithGoogle = async () => {
     try {
       const result = await signInWithPopup(auth, provider); //logged-in user's info stored in result
@@ -19,35 +20,39 @@ export const Auth = ({ setIsAuth, setRoom }) => {
       console.error(error); //trouble shooting
     }
   };
-
-  function signUpRedirect() {
-    window.location.href = "/signup"; //redirecting to signup page
+  const showSignUp = () => {
+    show = true;
   }
   return (
-    <div className="auth-container">
-      <header className="auth-header">
-        <h1>NormChat</h1>
-      </header>
-      <div className="auth-content">
-        <Card className="auth-card">
-          <div className="card-header">
-            <p className="pb-3 text-xl">Sign In</p>
-          </div>
-          <div className="card-content">
-            <button className="btn btn-success" onClick={signInWithGoogle}>Sign in with Google</button>
-            <div className="separator">or</div>
-            <form className="mb-1">
-              <input type="email" placeholder="Email" className="input input-bordered w-full max-w-xs mb-1 p-1.5 border rounded" />
-              <input type="password" placeholder="Password" className="input input-bordered w-full max-w-xs p-1.5 border rounded" />
-            </form>
-            <button className="btn btn-success btn-wide w-full mb-2.5">Sign in</button>
-            <button className="btn btn-success btn-wide w-full" onClick={signUpRedirect}>Sign up</button>
-          </div>
-          <div className="card-footer">
-            <p>Created by students for students to answer all UNC Charlotte-related questions.</p>
-          </div>
-        </Card>
+    <>
+    {!showSignUp && 
+      <div className="auth-container">
+        <header className="auth-header">
+          <h1>NormChat</h1>
+        </header>
+        <div className="auth-content">
+          <Card className="auth-card">
+            <div className="card-header">
+              <p className="pb-3 text-xl">Sign In</p>
+            </div>
+            <div className="card-content">
+              <button className="btn btn-success" onClick={signInWithGoogle}>Sign in with Google</button>
+              <div className="separator">or</div>
+              <form className="mb-1">
+                <input type="email" placeholder="Email" className="input input-bordered w-full max-w-xs mb-1 p-1.5 border rounded" />
+                <input type="password" placeholder="Password" className="input input-bordered w-full max-w-xs p-1.5 border rounded" />
+              </form>
+              <button className="btn btn-success btn-wide w-full mb-2.5">Sign in</button>
+              <button className="btn btn-success btn-wide w-full" onClick={ showSignUp }>Sign up</button>
+            </div>
+            <div className="card-footer">
+              <p>Created by students for students to answer all UNC Charlotte-related questions.</p>
+            </div>
+          </Card>
+        </div>
       </div>
-    </div>
+    }
+    {showSignUp && <Signup />}
+    </>
   );
 };
