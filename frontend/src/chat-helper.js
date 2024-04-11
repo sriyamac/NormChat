5a651e5aad0dd5b5903c13c5f30f3918860aa8e2
Giovanni A. <32625900+gabbate102@users.noreply.github.com>
2024-04-11 13:13:11 -0400

    Update chat-helper.js to use custom API endpoint

--- frontend/src/chat-helper.js
+++ frontend/src/chat-helper.js
@@ -1,3 +1,5 @@
-export function chat(user_message) {
-  
+export async function chat(user_message) {
+  const response = await fetch(""); // update this to use the custom api endpoint
+  const message = await response.json();
+  console.log(message);
 }
