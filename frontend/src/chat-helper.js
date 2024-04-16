6330250f17a7cc8b69a498d3d4668c6c059f69fa
Giovanni A. <32625900+gabbate102@users.noreply.github.com>
2024-04-16 19:02:23 -0400

    Update chat-helper.js to use JSON.stringify for request body

--- frontend/src/chat-helper.js
+++ frontend/src/chat-helper.js
@@ -2,13 +2,12 @@ export default async function chat(user_message) {
   let data = {"content":user_message};
   const response = await fetch("http://127.0.0.1:8000/chat", {
     method: "POST",
-    mode: "no-cors",
     headers: {
       "Content-Type": "application/json",
     },
-    body: data
+    body: JSON.stringify(data)
   }); // update this to use the custom api endpoint
-  const message = await response.json();
+  const message = await response.text();
   console.log(message);
   return message;
 }
