90cd0f4fdd593774ddefcab98956db841a675b3f
Giovanni A. <32625900+gabbate102@users.noreply.github.com>
2024-04-16 21:37:25 -0400

    added comments to main.py

--- server/main.py
+++ server/main.py
@@ -3,13 +3,16 @@ from pydantic import BaseModel
 from chatbot import Chatbot
 from fastapi.middleware.cors import CORSMiddleware
 
+# create FastAPI instance
 app = FastAPI()
 
+# Create Accepted Origins for CORS
 origins = [
     "http://localhost",
     "http://localhost:3000",
 ]
 
+# Add middleware for CORS 
 app.add_middleware(
     CORSMiddleware,
     allow_origins=origins,
@@ -18,13 +21,17 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
+# Create model for userRequest data
 class UserRequest(BaseModel):
     content: str
 
+# Default route
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
 
+# Chat API is used to get a response from chatbot.py. 
+# details about specifications can be found in backend_notes.md
 @app.post("/chat")
 async def chat(user_query: UserRequest):
     response = Chatbot.ask_openai(Chatbot,"hello",150)
