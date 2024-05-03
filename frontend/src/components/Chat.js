d02f94bc50ec7af528a63df3369e7db29b9d7b63
Giovanni A. <32625900+gabbate102@users.noreply.github.com>
2024-05-03 11:59:12 -0400

    Refactor chat page styling and add close chat button

--- frontend/src/components/Chat.js
+++ frontend/src/components/Chat.js
@@ -95,37 +95,40 @@ export const Chat = (isAuth) => {
       <div className="header">
         <div></div>
         <h1>Room ID: {room.toUpperCase()}</h1> {/**room ID is displayed for debugging purposes, will change to "NormChat" before deployment*/}
-        <Link to="/auth" className="header-right-col">
-          <button onClick={signUserOut} className='btn btn-active'> Sign Out </button>
-        </Link>
+        <div className="header-right-col header-button-container">
+          <Link to="/conversation-list">
+            <button className="btn btn-active">Close Chat</button>
+          </Link>
+          <Link to="/auth">
+            <button onClick={signUserOut} className='btn btn-active'> Sign Out </button>
+          </Link>
+        </div>
       </div>
 
       <div className="chat-container">
         <Card className="messagesCard grid grid-cols-1 grid-rows-11">
-          <div className="messages row-span-10">
+          <div className="messages row-span-10 overflow-y-scroll">
 
             {messages.map((message) => (
-              <div key={message.id} className="message">
+              <div key={message.id} className="m-2">
                 <span className="user">{message.user}:</span> {message.text}
               </div>
             ))}
 
           </div>
 
-          <form onSubmit={handleSubmit} className="new-message-form row-span-1 grid grid-cols-6">
+          <form onSubmit={handleSubmit} className="new-message-form row-span-1 grid grid-cols-6 gap-2">
             <input
               type="text"
               value={newMessage}
               onChange={(event) => setNewMessage(event.target.value)}
-              className="new-message-input col-span-5 m-1"
+              className="input input-bordered col-span-5"
               placeholder="Type your message here..."
             />
             <button type="submit" className="btn btn-success col-span-1">
               Send
             </button>
           </form >
-
-          <Link to="/conversation-list"><button className="btn btn-active">Close Chat</button></Link>
         </Card> </div>
     </div >
   );
