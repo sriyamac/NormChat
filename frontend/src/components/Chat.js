470de1ff11d0677d6ee2c1ae51298a42bb7be764
Giovanni A. <32625900+gabbate102@users.noreply.github.com>
2024-05-04 00:15:48 -0400

    Removed initialize chat

--- frontend/src/components/Chat.js
+++ frontend/src/components/Chat.js
@@ -41,11 +41,17 @@ export const Chat = (isAuth) => {
       setMessages(messages);
     });
 
+    if (messages.length == 0) {
+      initializeChat();
+      console.log("initializeChat");
+    }
+
     return () => unsuscribe(); //cleaning up function
   }, []);
 
   // initialize displays a welcome message 
   const initializeChat = async (e) => {
+    console.log("initializeChat");
     // add message to messages array
     await addDoc(messagesRef, {
       text: "Hello! How can I help you with University of North Carolina at Charlotte information?", //content of the message
